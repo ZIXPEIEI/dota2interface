eb4fb95d859eedb312bd47b81feb03f709fd8e29
ZIXPEIEI <tarwannamtan@gmail.com>
2020-09-16 11:20:29 +0700

    first commit

---
+++ src/main/java/com/gtc/dota2interface/Dota2interfaceApplication.java
@@ -0,0 +1,13 @@
+package com.gtc.dota2interface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dota2interfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dota2interfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtc/dota2interface/Dota2interfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtc.dota2interface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dota2interfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
